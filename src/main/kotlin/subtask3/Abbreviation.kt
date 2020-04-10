1b29b76ac622b4299b89913e1193e7692278ecb1
Dzmitry_Kavaliou4 <dzmitry_kavaliou4@epam.com>
2020-04-10 18:39:45 +0300

    task3

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -2,8 +2,30 @@ package subtask3
 
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val regex = buildRegex(b)
+
+        return if (a.matches(regex)) "YES" else "NO"
+    }
+
+    private fun buildRegex(word: String): Regex {
+
+        val stringBuilder = StringBuilder()
+
+        with(stringBuilder) {
+            for (c in word) {
+                append(ZERO_OR_MORE_CHARACTERS_REGEX)
+                append(c)
+            }
+            append(ZERO_OR_MORE_CHARACTERS_REGEX)
+        }
+
+
+        return "$stringBuilder".toRegex(RegexOption.IGNORE_CASE)
+    }
+
+    companion object {
+        const val ZERO_OR_MORE_CHARACTERS_REGEX = ".*"
     }
 }
