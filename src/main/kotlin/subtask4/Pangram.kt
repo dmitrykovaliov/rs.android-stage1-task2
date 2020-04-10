31c38e8b9ccf3074d53194e5bd6d5640e3d180eb
Dzmitry_Kavaliou4 <dzmitry_kavaliou4@epam.com>
2020-04-10 18:40:44 +0300

    task4

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -2,8 +2,70 @@ package subtask4
 
 class Pangram {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val operationalString = inputString.trim()
+
+        if (operationalString.isEmpty()) {
+            return ""
+        }
+        return if (isPangram(operationalString)) fetchResult(
+            operationalString,
+            LettersType.VOWELS
+        ) else fetchResult(operationalString)
+    }
+
+    private fun fetchResult(
+        input: String,
+        usedLetters: LettersType = LettersType.CONSONANTS
+    ): String {
+
+        return input.split(SPACES_REGEX.toRegex())
+            .map { word ->
+                var vowelsCount = 0
+                val stringBuilder = StringBuilder()
+
+                for (c in word.trim()) {
+                    stringBuilder.append(
+                        if (usedLetters.lettersSet
+                                .contains(c, true)
+                        ) {
+                            vowelsCount++
+                            c.toUpperCase()
+                        } else {
+                            c
+                        }
+                    )
+                }
+                stringBuilder.insert(0, vowelsCount).toString()
+            }
+            .sortedBy { it.first() }
+            .joinToString(" ")
+    }
+
+    private fun isPangram(input: String): Boolean {
+
+        val lettersSet = mutableSetOf<String>()
+        for (c in input.toLowerCase()) {
+            if (c in LettersType.ALL.lettersSet) {
+                lettersSet.add(c.toString())
+            }
+        }
+        val joinedString = lettersSet
+            .sorted()
+            .joinToString("")
+
+        return joinedString == LettersType.ALL.lettersSet
+    }
+
+    private enum class LettersType(val lettersSet: String) {
+
+        ALL("abcdefghijklmnopqrstuvwxyz"),
+        VOWELS("aeiouy"),
+        CONSONANTS("bcdfghjklmnpqrstvwxz")
+    }
+
+    companion object {
+        const val SPACES_REGEX = "\\s+"
     }
 }
