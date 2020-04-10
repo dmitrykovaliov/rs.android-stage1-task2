5d7fcadc09da9bc9c65b0a61d48593b8dc29523d
Dzmitry_Kavaliou4 <dzmitry_kavaliou4@epam.com>
2020-04-10 18:40:56 +0300

    task5

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,25 @@
 package subtask5
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
+    @Suppress("UNCHECKED_CAST")
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+
+        val filteredResult = blockA.filter { it.javaClass == blockB.javaObjectType }
+        if (filteredResult.isEmpty()) {
+            return ""
+        }
+        return when (blockB) {
+            Int::class -> (filteredResult as ArrayList<Int>).sum()
+            LocalDate::class -> {
+                val maxDate = (filteredResult as ArrayList<LocalDate>).max()!!
+                maxDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+            }
+            else -> filteredResult.joinToString("")
+        }
     }
 }
